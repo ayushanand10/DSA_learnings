0a5fc2acf9040b2e8e4f23e3ff1df4615fe3f9c6
Ayush <ayushanand1010@gmail.com>
2022-02-17 17:28:04 +0530

    Creating a linklist and displaying it's contents in iterative way

---
+++ LinkList/iterativeLinklist.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+} *first = NULL;
+
+class linkList
+{
+public:
+    void create(int A[], int n)
+    {
+        Node *last, *t;
+        first = new Node;
+        first->data = A[0];
+        first->next = NULL;
+        last = first;
+        for (int i = 1; i < n; i++)
+        {
+            t = new Node;
+            t->data = A[i];
+            // t->next = NULL;
+            last->next = t;
+            last = t;
+        }
+        last->next = NULL;
+    }
+    void displayIterative()
+    {
+        Node *t;
+        t = first;
+        while (t)
+        {
+            cout << t->data << " ";
+            t = t->next;
+        }
+    }
+};
+
+int main()
+{
+    linkList p;
+    int A[] = {1, 62, 13, 9, 5}, n = 5;
+    p.create(A, n);
+    p.displayIterative();
+    return 0;
+}
